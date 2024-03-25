77bb3988b697d3a4fe38503217056c8eee1be2c0
Yurii Filiushkin <yurii.filiushkin@gmail.com>
2024-03-25 12:02:01 +0200

    make build

---
+++ index.b7deeaf2.js
@@ -0,0 +1,2 @@
+var e=document.documentElement;document.querySelector(".header__link--menu"),window.addEventListener("hashchange",function(){"#menu"===window.location.hash?e.classList.add("page__menu-active"):e.classList.remove("page__menu-active")});
+//# sourceMappingURL=index.b7deeaf2.js.map
